e26a06e90de98e44168dd43662d17487811f3240
Gearhead2022 <140861950+Gearhead2022@users.noreply.github.com>
2024-03-21 10:55:49 +0800

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path
+from . import views
+from .views import index,display_qr_list,webcam_qr_code_scanner,fetch_messages,display_current_time
+# from .app_views.qr_generator import generate_qr_code,user_profile
+from django.conf.urls.static import static
+from django.conf import settings
+from .app_views.export import export,export_data_afternoon,view_attendance
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
+from .app_views.addemployee import addemployee
+
+
+urlpatterns = [
+    path('', views.login_view, name='login'),
+    path('home/', views.home, name='home'),
+    #path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('logout/', views.custom_logout, name='logout'),
+    path('index/', index, name="index"),
+    path('display_qr_list/', display_qr_list, name='display_qr_list'),
+    path('webcam_qr_code_scanner/',webcam_qr_code_scanner,name='webcam_qr_code_scanner'),
+    path('fetch_messages/', fetch_messages, name='fetch_messages'),
+    # path('QR_list/', generate_qr_code, name='generate_qr_code'),
+    # path('user_profile/<int:pk>/', user_profile, name='user_profile'),
+    path('export/', export, name='export'),
+    path('export_all/', export_data_afternoon, name='export_data_afternoon'),
+    path('addemployee/', addemployee, name='addemployee'),
+    path('view_attendance/', view_attendance, name='view_attendance'),
+    path('display_current_time/', display_current_time, name='display_current_time'),
+
+
+]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
